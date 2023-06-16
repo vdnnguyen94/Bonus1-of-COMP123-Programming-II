a8fa23e4f10edbfffdb050e665cdf6ead5801308
vdnnguyen94 <78767292+vdnnguyen94@users.noreply.github.com>
2023-06-16 18:44:46 -0400

    Add files via upload

---
+++ Bonus2/Plane.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus2
+{
+    class Plane
+    {
+        string Manufacture { get; set; }
+        string Model { get; set; }
+        int Capacity { get; set; }
+        string Type { get; set; }
+        string SerialNumber { get; set; }
+        public Plane()
+        {
+            Manufacture = "unknown";
+            Model = "unknown";
+            Capacity = 0;
+            Type = "unassigned";
+            SerialNumber = "00000";
+        }
+        public Plane(string manufacture, string model, int capacity, string type, string serialNumber)
+        {
+            Manufacture = manufacture;
+            Model = model;
+            Capacity = capacity;
+            Type = type;
+            SerialNumber = serialNumber;
+        }
+        public override string ToString()
+        {
+            return $"Manufacture: {Manufacture}, Model: {Model}, Capacity: {Capacity}, Type: {Type}, SerialNumber: {SerialNumber}.";
+        }
+        public bool HasCapcity(int capacity)
+        {
+
+            return (Capacity >= capacity) ? true : false;
+        }
+    }
+}
---
+++ Bonus2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Plane> PlaneList = new List<Plane>();
+            PlaneList.Add(new Plane("Boeing", "Sx500 ", 450, "Passenger", "02145"));
+            PlaneList.Add(new Plane("Airbus","02ZD",255,"Passenger","24AD230"));
+            PlaneList.Add(new Plane("Delta", "Fighter-Jet ", 2, "Military", "X0015"));
+            PlaneList.Add(new Plane("United AirCraft", "UACCargo05", 60, "Cargo", "89D220"));
+            PlaneList.Add(new Plane());
+            int[] capacityList = new int[] { 20, 50, 100, 300, 500 } ;
+            Console.WriteLine("Plane in the List");
+            foreach (Plane plane in PlaneList)
+            {
+                Console.WriteLine((PlaneList.IndexOf(plane) + 1).ToString() + " " + plane.ToString());
+            }
+            do
+            {
+                Console.Write("Enter 0 to Exit\nOr Enter the Index of Plane you would like to check Capacity(1-5)\nYour choice:    ");
+                string choice = Console.ReadLine();
+                string Capacity = "";
+                string NoCapacity = "";
+                int i = 0;
+                int y = 0;
+                if (choice == "0") { break; }
+                else if (Convert.ToInt32(choice) >= 1 && Convert.ToInt32(choice) <= PlaneList.Count)
+                {
+                    int planeIndex = Convert.ToInt32(choice) -1;
+                    foreach (int capacity in capacityList)
+                    {
+                        if (PlaneList[planeIndex].HasCapcity(capacity))
+                        { if (i == 0) { Capacity = capacity.ToString(); i++; }
+                            else { Capacity = Capacity + "," + capacity.ToString(); } }
+                        else { if (y == 0) { NoCapacity = capacity.ToString(); y++; }
+                            else { NoCapacity = NoCapacity + "," + capacity.ToString(); } }
+                    }
+                    string[] CapacityNumber = Capacity.Split(',');
+                    string[] NoCapacityNumber = NoCapacity.Split(',');
+                    Console.WriteLine("This Plane can have space for : "+string.Join(",",CapacityNumber));
+                    Console.WriteLine("This Plane DOES NOT  have space for : " + string.Join(",", NoCapacityNumber));
+                }
+                else { Console.WriteLine("INVALID CHOICE"); }
+            }
+            while (true);
+        }
+    }
+}
